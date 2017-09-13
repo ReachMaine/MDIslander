1dff6064c1a96565d3626e9bb558e81d4a7094da
Linda "zig" Speight <linda@reachmaine.com>
2017-09-13 14:15:29 -0400

    switching to newer tecnavia scripts

--- origmag-mdi/footer.php
+++ origmag-mdi/footer.php
@@ -1,8 +1,9 @@
-<?php /* 
+<?php /*
 	modifications:
-		10Oct14 - zig - modify copyright  
+		10Oct14 - zig - modify copyright
 		24Dec14 - zig - add broadstreet popup script
 		31jan16 - zig - add technavia script at bottom of page
+		13sept17 - zig - changing technavia script to newer version
 */ ?>
 <?php global $theme_url, $prl_data;?>
 	<footer id="footer">
@@ -27,14 +28,14 @@
 				<div class="right"><?php echo trim($prl_data['footer_text']);?></div>
 			</div>
 		</div><!-- .copyright -->
-		
+
 	</footer><!-- #footer -->
-	
+
 </div><!-- .site-wrapper -->
-	<?php if($prl_data['switcher']=='Enable') include ('_switcher/index .php');?>  
+	<?php if($prl_data['switcher']=='Enable') include ('_switcher/index .php');?>
 	<a id="toTop" href="#"><i class="fa fa-long-arrow-up"></i></a>
-<script data-cfasync="false" type="text/javascript" src="http://mountdesertislander.me.pw.newsmemory.com/include.php"></script>
-	<?php /* technavia footer script */ 
+	<script data-cfasync="false" type="text/javascript" src="http://mountdesertislander.me.pw.newsmemory.com/?meter&amp;v=0"></script>
+	<?php /* technavia footer script */
 	if (!is_singular('post')) { 	?>
 	<?php }  ?>
 	<?php wp_footer();?>
