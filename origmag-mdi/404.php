01f0c4f768fbec3369b74748cbfc32a5de380b31
linda@reachmaine.com <linda@reachmaine.com>
2019-09-17 08:22:41 -0400

    take out extraneous GA codes

--- origmag-mdi/404.php
+++ origmag-mdi/404.php
@@ -1,13 +1,13 @@
 <?php get_header();?>
 <div class="prl-container">
     <div class="prl-grid prl-grid-divider">
-		<section id="main" class="prl-span-12"> 
+		<section id="main" class="prl-span-12">
 			<h1><?php _e('Hmm, we can\'t find the page you\'re looking for.','presslayer');?></h1>
 			<h4><?php _e('Something went wrong and we apologize.','presslayer');?>.</h4>
 			<p><?php _e('Did you try searching? Click the search icon above and enter one or more keywords. Or try one of the links below in the page footer.','presslayer');?>.</p>
                         <p><script type="text/javascript">
   var GOOG_FIXURL_LANG = 'en';
-  var GOOG_FIXURL_SITE = 'http://www.mdislander.com/'
+  var GOOG_FIXURL_SITE = 'https://www.mdislander.com/'
 </script>
 <script type="text/javascript"
   src="http://linkhelp.clients.google.com/tbproxy/lh/wm/fixurl.js">
@@ -16,29 +16,4 @@
 
     </div><!--.prl-grid-->
 </div>
-<?php get_footer();?>    
-
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-55634975-3', 'auto');
-  ga('send', 'pageview', '/404/mdi/404.html?page='+ document.location.pathname + document.location.search +'&from=' + document.referrer);    
-
-</script>
-
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-55634975-4', 'auto');
-  ga('send', 'pageview', '/404/?page='+ document.location.pathname + document.location.search +'&from=' + document.referrer);    
-
-</script>   
-
-
-
+<?php get_footer();?>
