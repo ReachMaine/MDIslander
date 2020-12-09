a1b69373aab081f23a0b764c8efb55044e96deb6
Linda "zig" Speight <linda@reachmaine.com>
2020-12-09 11:03:59 -0500

    Fix paywall on non-metered pages
    s.t. it shows login and not ~

--- origmag-mdi/header.php
+++ origmag-mdi/header.php
@@ -54,6 +54,9 @@ $body_class = array('Boxed'=>'site-boxed', 'Wide'=>'site-wide');
 ?>
 <body <?php body_class($body_class[$prl_data['site_style']]); ?>>
 <?php if ( function_exists( 'gtm4wp_the_gtm_tag' ) ) { gtm4wp_the_gtm_tag(); } ?>
+<?php if ( !is_user_logged_in() ) { ?>
+	<script data-cfasync="false" type="text/javascript" src="//ellsworthamerican-me-pw.newsmemory.com/?meter&amp;v=0"></script>
+<?php } ?>
 <div id="topbar"><?php if ( is_active_sidebar( 'topbar') ) { 	dynamic_sidebar( 'topbar' );	}  ?></div>
 <div class="site-wrapper">
     <!--<div class="prl-container">-->
